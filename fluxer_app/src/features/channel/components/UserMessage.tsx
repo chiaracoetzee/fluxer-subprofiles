c28cfb3321f87f4410e1583583652c0b200486d0
Chiara Coetzee <chiaracoetzee@gmail.com>
2026-09-09 14:48:49 -0700

    feat(persona): remove via text from root account indicator
    - Remove via text before root account avatar on persona messages
    
    - Add compact margin-inline-start spacing to root account avatar
    
    - Render root account avatar on contentless persona messages

--- fluxer_app/src/features/channel/components/UserMessage.tsx
+++ fluxer_app/src/features/channel/components/UserMessage.tsx
@@ -582,17 +582,14 @@ export const UserMessage = observer(() => {
 										previewName={previewOverrides?.displayName}
 										data-flx="channel.user-message.message-username--2"
 									/>
-									{/* TODO: via + user icon here */}
 									<MessagePersonaAccount
 										user={author}
 										message={message}
 										guild={guild}
 										member={member ?? undefined}
-										className={styles.messageUsername}
 										isPreview={!!previewContext}
 										previewColor={previewOverrides?.usernameColor}
 										previewName={previewOverrides?.displayName}
-										data-flx="channel.user-message.message-username--2"
 									/>
 									{author.bot && (
 										<UserTag
@@ -692,6 +689,15 @@ export const UserMessage = observer(() => {
 									previewColor={previewOverrides?.usernameColor}
 									previewName={previewOverrides?.displayName}
 									data-flx="channel.user-message.message-username--3"
+								/>
+								<MessagePersonaAccount
+									user={author}
+									message={message}
+									guild={guild}
+									member={member ?? undefined}
+									isPreview={!!previewContext}
+									previewColor={previewOverrides?.usernameColor}
+									previewName={previewOverrides?.displayName}
 								/>
 								{author.bot && (
 									<UserTag
